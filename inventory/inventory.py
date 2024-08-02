c42e8567178791e60db58a9f7f35aba06dd5f2f4
Rian <rlina1785@gmail.com>
2024-08-01 21:03:53 -0300

    deleted applications, and start now new project

--- inventory/inventory.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/stock/', consumers.StockConsumer.as_asgi()),
-]
--- inventory/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField()
-
-    def __str__(self):
-        return self.name
